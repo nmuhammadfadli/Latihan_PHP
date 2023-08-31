a36901a7865dae6b23aa8b0d41ed3ad42c3cb98b
nmuhammadfadli <124648757+nmuhammadfadli@users.noreply.github.com>
2023-08-31 12:46:06 +0700

    Perhitungan sederhana

---
+++ latihan1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kalkulator Pertambahan</title>
+</head>
+<body>
+    <h2>Kalkulator Pertambahan Sederhana</h2>
+    <form method="post" action="">
+        Angka 1: <input type="number" name="angka1" required><br>
+        Angka 2: <input type="number" name="angka2" required><br>
+        <input type="submit" name="submit" value="Hitung">
+    </form>
+
+    <?php
+    if(isset($_POST['submit'])){
+        $angka1 = $_POST['angka1'];
+        $angka2 = $_POST['angka2'];
+        $hasil = $angka1 + $angka2;
+        echo "<p>Hasil pertambahan: $hasil</p>";
+    }
+    ?>
+</body>
+</html>
